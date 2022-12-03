37e7d0e6c7dbda637a1b81d26e7eaaecd81f9f9e
Carl Tsai <dauthdaert@gmail.com>
2022-12-02 23:24:04 -0800

    tidy things up

--- 1/1_1.py
+++ 1/1_1.py
@@ -1,12 +1,16 @@
-elves = []
-calories = []
+def main():    
+    elves = []
+    calories = []
 
-with open("1/input.txt") as f:
-    for line in f:
-        if(line.rstrip() == ""):
-            elves.append(calories)
-            calories = []
-        else:
-            calories.append(int(line))
+    with open("1/input") as f:
+        for line in f:
+            if(line.rstrip() == ""):
+                elves.append(calories)
+                calories = []
+            else:
+                calories.append(int(line))
 
-print(max([sum(l) for l in elves]))
+    print(max([sum(l) for l in elves]))
+
+if __name__ == "__main__":
+    main()
--- 1/1_2.py
+++ 1/1_2.py
@@ -1,12 +1,16 @@
-elves = []
-calories = []
+def main():
+    elves = []
+    calories = []
 
-with open("1/input.txt") as f:
-    for line in f:
-        if(line.rstrip() == ""):
-            elves.append(calories)
-            calories = []
-        else:
-            calories.append(int(line))
+    with open("1/input") as f:
+        for line in f:
+            if(line.rstrip() == ""):
+                elves.append(calories)
+                calories = []
+            else:
+                calories.append(int(line))
 
-print(sum((sorted([sum(e) for e in elves])[-3:])))
+    print(sum((sorted([sum(e) for e in elves])[-3:])))
+
+if __name__ == "__main__":
+    main()
