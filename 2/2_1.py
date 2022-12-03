5237ac39c4974f942529ed9526bcedcfaacd5356
Carl Tsai <dauthdaert@gmail.com>
2022-12-02 23:24:12 -0800

    Day 2

---
+++ 2/2_1.py
@@ -0,0 +1,25 @@
+def main():
+    scores = [[4, 8, 3],
+              [1, 5, 9],
+              [7, 2, 6]]
+
+    mapping = {
+        "X": 0,
+        "Y": 1,
+        "Z": 2,
+        "A": 0,
+        "B": 1,
+        "C": 2,
+    }
+
+    total = 0
+
+    with open("2/input") as f:
+        for line in f:
+            first, second = [mapping[c] for c in line.split()]
+            total += scores[first][second]
+    
+    print(total)
+
+if __name__ == "__main__":
+    main()
