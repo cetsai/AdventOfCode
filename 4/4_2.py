d059d300346d66a4df27518d50a79d8cfe1cc3d1
Carl Tsai <dauthdaert@gmail.com>
2022-12-04 00:08:57 -0800

    Day 4

---
+++ 4/4_2.py
@@ -0,0 +1,15 @@
+def main():
+    total = 0
+    with open("4/input") as f:
+        for line in f:
+            range1, range2 = line.rstrip().split(",")
+            min1, max1 = [int(s) for s in range1.split("-")]
+            min2, max2 = [int(s) for s in range2.split("-")]
+
+            if (not (max1 < min2 or min1 > max2)):
+                total += 1
+    
+    print(total)
+
+if __name__ == "__main__":
+    main()
