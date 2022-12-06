fbe9a5b704a2d3707bcdc0b6aae293801a6089d6
Carl Tsai <dauthdaert@gmail.com>
2022-12-05 17:37:02 -0800

    Day 5

---
+++ 5/5_2.py
@@ -0,0 +1,41 @@
+import re
+
+def readInput(stacks, commands):
+    with open("5/input") as f:
+        lines = iter(f)
+        for line in map(str.rstrip, lines):
+            if not line:
+                break
+            for index, char in enumerate(line):
+                if not char.isalpha():
+                    continue
+                stack = (index // 4) + 1
+                if not stack in stacks:
+                    stacks[stack] = []
+                stacks[stack].append(char)
+
+        for stack in stacks.values():
+            stack.reverse()
+            
+        for line in map(str.rstrip, lines):
+            commandregex = re.compile(r"move ([0-9]+) from ([0-9]+) to ([0-9]+)")
+            match = commandregex.fullmatch(line)
+            assert match
+            commands.append(tuple(map(int, match.groups())))
+
+def main():
+    stacks = {}
+    commands = []
+
+    readInput(stacks, commands)
+
+    for num, source, dest in commands:
+        stacks[dest].extend(list(stacks[source][-num:]))
+        del stacks[source][-num:]
+
+    stacks = dict(sorted(stacks.items()))
+    message = "".join(stack.pop() for stack in stacks.values())
+    print(message)
+
+if __name__ == "__main__":
+    main()
