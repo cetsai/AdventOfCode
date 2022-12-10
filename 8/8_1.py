bb063422f4d09b2f3b00a8ffa6c43ed063100690
Carl Tsai <dauthdaert@gmail.com>
2022-12-09 16:15:00 -0800

    Day 8

---
+++ 8/8_1.py
@@ -0,0 +1,39 @@
+def main():
+    grid = []
+    with open("8/input") as f:
+        for line in f:
+            row = []
+            for char in line.rstrip():
+                row.append(int(char))
+            grid.append(row)
+
+    height = len(grid)
+    width = len(grid[0])
+    visibility = [[0] * width for _ in range(height)]
+
+    
+    umax = [-1] * width
+    dmax = [-1] * width
+    for i in range(height):
+        lmax = -1
+        rmax = -1
+        for j in range(width):
+            ultree = grid[i][j]
+            drtree = grid[-i-1][-j-1]
+            if ultree > umax[j]:
+                umax[j] = ultree
+                visibility[i][j] = 1
+            if ultree > lmax:
+                lmax = ultree
+                visibility[i][j] = 1
+            if drtree > dmax[j]:
+                dmax[j] = drtree
+                visibility[-i-1][-j-1] = 1
+            if drtree > rmax:
+                rmax = drtree
+                visibility[-i-1][-j-1] = 1
+        
+    print(sum(sum(tree) for tree in visibility))
+
+if __name__ == "__main__":
+    main()
---
+++ 8/8_2.py
@@ -0,0 +1,32 @@
+def main():
+    grid = []
+    with open("8/input") as f:
+        for line in f:
+            row = []
+            for char in line.rstrip():
+                row.append(int(char))
+            grid.append(row)
+
+    height = len(grid)
+    width = len(grid[0])
+    scores = [[1] * width for _ in range(height)]
+    views = [[0] * 10 for _ in range(4)]
+
+    for idx in range(width * height):
+        for direction in range(len(views)):
+            coord = [0,0]
+            dim = height if direction // 2 == 0 else width
+            coord[0] = idx // width if direction // 2 == 0 else idx % height
+            coord[1] = idx % width if direction // 2 == 0 else idx // height
+            coord[0] = coord[0] if direction % 2 == 0 else -coord[0] - 1
+            coord[1] = coord[1] if direction % 2 == 0 else -coord[1] - 1
+            tree = grid[coord[0]][coord[1]]
+            if idx % dim == 0:
+                views[direction] = [0] * 10
+            scores[coord[0]][coord[1]] *= views[direction][tree]
+            views[direction] = [v + 1 if i > tree else 1 for i, v in enumerate(views[direction])]
+    
+    print(max([max(s) for s in scores]))
+
+if __name__ == "__main__":
+    main()
