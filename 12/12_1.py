072616ee611d2b9ef7a14de42251d9286f60296b
Carl Tsai <dauthdaert@gmail.com>
2022-12-14 02:15:38 -0800

    Day 12

---
+++ 12/12_1.py
@@ -0,0 +1,50 @@
+import numpy as np
+from collections import deque
+
+def main():
+    input_map = []
+    start = None
+    end = None
+    with open("12/input") as f:
+        for x, line in enumerate(f):
+            row = []
+            for y, c in enumerate(line.rstrip()):
+                if c == "S":
+                    start = (x, y)
+                    row.append(ord('a') - ord('a'))
+                elif c == "E":
+                    end = (x, y)
+                    row.append(ord('z') - ord('a'))
+                else:
+                    row.append(ord(c) - ord('a'))
+            input_map.append(row)
+    
+    elevation_map = np.array(input_map)
+    distance_map = np.full(elevation_map.shape, -1)
+    neighbor_offsets = np.array([[-1, 0], [1,0], [0,-1], [0,1]])
+    
+    horizon = deque()
+    horizon.append(start)
+    distance_map[start] = 0
+    while True:
+        location = horizon.popleft()
+        if np.all(location == end):
+            break
+
+        neighbors = np.array(location) + neighbor_offsets
+        for neighbor in neighbors:
+            if np.any(neighbor < 0) or np.any(neighbor >= np.array(distance_map.shape)):
+                continue
+            if distance_map[tuple(neighbor)] > 0:
+                continue
+            if elevation_map[tuple(neighbor)] <= elevation_map[location] + 1:
+                horizon.append(tuple(neighbor))
+                distance_map[tuple(neighbor)] = distance_map[location] + 1
+        
+        if not horizon:
+            break
+
+    print(distance_map[end])
+
+if __name__ == "__main__":
+    main()
---
+++ 12/12_2.py
@@ -0,0 +1,51 @@
+import numpy as np
+from collections import deque
+
+def main():
+    input_map = []
+    end = None
+    with open("12/input") as f:
+        for x, line in enumerate(f):
+            row = []
+            for y, c in enumerate(line.rstrip()):
+                if c == "S":
+                    start = (x, y)
+                    row.append(ord('a') - ord('a'))
+                elif c == "E":
+                    end = (x, y)
+                    row.append(ord('z') - ord('a'))
+                else:
+                    row.append(ord(c) - ord('a'))
+            input_map.append(row)
+    
+    elevation_map = np.array(input_map)
+    distance_map = np.full(elevation_map.shape, -1)
+    neighbor_offsets = np.array([[-1, 0], [1,0], [0,-1], [0,1]])
+    
+    horizon = deque()
+    horizon.append(end)
+    distance_map[end] = 0
+    start = None
+    while True:
+        location = horizon.popleft()
+        if not elevation_map[location]:
+            start = location
+            break
+
+        neighbors = np.array(location) + neighbor_offsets
+        for neighbor in neighbors:
+            if np.any(neighbor < 0) or np.any(neighbor >= np.array(distance_map.shape)):
+                continue
+            if distance_map[tuple(neighbor)] > 0:
+                continue
+            if elevation_map[tuple(neighbor)] >= elevation_map[location] - 1:
+                horizon.append(tuple(neighbor))
+                distance_map[tuple(neighbor)] = distance_map[location] + 1
+        
+        if not horizon:
+            break
+
+    print(distance_map[start])
+
+if __name__ == "__main__":
+    main()
