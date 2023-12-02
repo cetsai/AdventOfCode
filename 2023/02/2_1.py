55226ccd1767dae301964aa04c8e841ec36bb674
Carl Tsai <dauthdaert@gmail.com>
2023-12-02 07:27:56 -0800

    remove unused variable

--- 2023/02/2_1.py
+++ 2023/02/2_1.py
@@ -1,13 +1,16 @@
 """
 https://adventofcode.com/2023/day/2
 
-Each line of the input describes one game in which cubes of 3 colors are
+Each line of the input describes one game, in which colored cubes are 
 drawn out of a bag. The task is to find which games are possible with a 
 bag containing 12 red, 13 green, and 14 blue cubes. The final answer is
 the sum of the game numbers of these 'possible' games.
 
 This is a simple exercise in parsing the input string and comparing the
-numbers drawn for each color with the total of that color in the bag.
+numbers drawn for each color with the total of that color in the bag. 
+The input separates the draws into sets, where each set can have a draw
+for each color, but this has no impact on the result, so we can treat
+draws as independent.
 """
 import re
 
@@ -26,8 +29,8 @@ def main():
     with open("input") as f:
         for line in f:
             game = int(game_pattern.match(line)[1])
-            draw = draw_pattern.findall(line)
-            for num, color in draw:
+            draws = draw_pattern.findall(line)
+            for num, color in draws:
                 if int(num) > bag[color]:
                     break
             else:
--- 2023/02/2_2.py
+++ 2023/02/2_2.py
@@ -6,7 +6,7 @@ mimimal set to get the 'power' of the set. The final answer is the sum
 of the powers of all games.
 
 The solution is similar to part one, except instead of comparing to a
-set value, we keep track of the minimal set for each game. As the input
+set value, we keep track of the minimal set for each game. As each draw
 is read, we check against this minimal set and update if needed. After
 each line, we find the power and add it to the result.
 """
@@ -15,7 +15,6 @@ from functools import reduce
 
 
 def main():
-    game_pattern = re.compile(r"Game (\d+): ")
     draw_pattern = re.compile(r"(?:(?P<num>\d+) "
                               r"(?P<color>(?:red)|(?:blue)|(?:green)))+")
     sum = 0
@@ -23,9 +22,8 @@ def main():
     with open("input") as f:
         for line in f:
             bag = {"red": 0, "green": 0, "blue": 0}
-            game = int(game_pattern.match(line)[1])
-            draw = draw_pattern.findall(line)
-            for num_str, color in draw:
+            draws = draw_pattern.findall(line)
+            for num_str, color in draws:
                 num = int(num_str)
                 if int(num) > bag[color]:
                     bag[color] = num
